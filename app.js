5d6e84b4dcbd0148a7b23aa6e69204cc3c978076
Jwnrobinson <89938078+Jwnrobinson@users.noreply.github.com>
2021-09-09 13:52:18 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+Let friends = [james, milo, jack, steve, sarah];
+
+for (let f = 0; f < friends.length; f++) {
+    const element = array[i]; {
+        for (let index = 0; index < array.length; index++) {
+            const element = array[index];
+            
+        }
+    }
+            
+}
+
+console.log(friends);
